04e325a2a355836372c361358aebac534cf14bb0
zhaoyangmushiyi <zhaoyangmushiyi@gmail.com>
2018-05-06 20:06:07 +0800

    Create CategoryServlet.

---
+++ imitateTmall/src/tmall/servlet/CategoryServlet.java
@@ -0,0 +1,75 @@
+package tmall.servlet;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import tmall.bean.Category;
+import tmall.util.ImageUtil;
+import tmall.util.Page;
+
+public class CategoryServlet extends BaseBackServlet {
+
+	@Override
+	public String add(HttpServletRequest request, HttpServletResponse response, Page page) {
+		Map<String, String> params = new HashMap<>();
+		InputStream is = super.parseUpload(request, params);
+		String name = params.get("name");
+		Category c = new Category();
+		c.setName(name);
+		categoryDAO.add(c);
+		File imageFolder = new File(request.getSession().getServletContext().getRealPath("img/category"));
+        File file = new File(imageFolder,c.getId()+".jpg");
+        try {
+			if (is == null && is.available() != 0) {
+				try (FileOutputStream fos = new FileOutputStream(file)){
+					byte b[] = new byte[1024 * 1024];
+					int length = 0;
+					while ((length = is.read(b)) != -1) {
+						fos.write(b, 0, length);
+					}
+					fos.close();
+					BufferedImage img = ImageUtil.change2jpg(file);
+					ImageIO.write(img, "jpg", file);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        return "@admin_category_list";
+	}
+
+	@Override
+	public String delete(HttpServletRequest request, HttpServletResponse response, Page page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String edit(HttpServletRequest request, HttpServletResponse response, Page page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String update(HttpServletRequest request, HttpServletResponse response, Page page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String list(HttpServletRequest request, HttpServletResponse response, Page page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ imitateTmall/src/tmall/util/ImageUtil.java
@@ -0,0 +1,13 @@
+package tmall.util;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class ImageUtil {
+
+	public static BufferedImage change2jpg(File file) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
